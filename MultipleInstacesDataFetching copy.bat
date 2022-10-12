a38bd6487e237b9db1ffe0460494ca6475668896
HMarmottant <113462699+HMarmottant@users.noreply.github.com>
2022-10-12 12:37:00 +0200

    Player history

---
+++ MultipleInstacesDataFetching copy.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set /A instances=19
+set /A offset=80000000
+set /A step=5000
+
+rem python might not be in your path as py you can replace it with the direct path to your python.exe for example
+
+FOR /L %%v IN (0 1 %instances% ) DO  (
+    start /B py .\DataFetching.py %offset% %step% %%v
+)
+
